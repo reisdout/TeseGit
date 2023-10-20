57b4371128d28b4178056f04e3b03a81a0b0f14a
reisdout <reisdout@gmail.com>
2023-10-20 18:26:04 -0300

    melhoras Gráfico ROC

--- Programas-Notebooks/Federated-LSTM-Shallow/apps/SoftmaxProj/seabornPlots.py
+++ Programas-Notebooks/Federated-LSTM-Shallow/apps/SoftmaxProj/seabornPlots.py
@@ -26,14 +26,14 @@ descricao_experimeto = ["5","10","20","30","40"]
 desloc_x_text=[0,-0.0007,0,0,0]
 desloc_y_text=[-0.1,-0.06,-0.1,-0.07,-0.1]
 
-x_bisse = np.array([0,0.003,0.006,0.010,0.013])
-y_bisse = np.array([0,0.003,0.006,0.010,0.013])
+x_bisse = np.array([0,0.003,0.006,0.010,0.0126050420168067])
+y_bisse = np.array([0,0.003,0.006,0.010,0.0126050420168067])
 # Create the pandas DataFrame 
 df = pd.DataFrame(data) 
 fig = plt.figure(figsize=(20,20))
 # print dataframe. 
 fig, ax = plt.subplots()
-ax.set_xlim(left=-0.002, right=0.015)
+ax.set_xlim(left=-0.0015, right=0.015)
 ax.set_ylim(bottom=-0.020, top=1.05)
 
 custom_params = {"axes.spines.right": False, "axes.spines.top": False}
@@ -44,12 +44,12 @@ sns.set_theme(style="whitegrid", palette="pastel")
 
 sns.axes_style("ticks")
 
-roc = sns.lineplot(df,x="FPR",y="TPR", label= 'ROCXFluxos',color ='blue', linewidth=1.5,ax=ax).set(title="ROC - Modelo 20 Fluxos")
+roc = sns.lineplot(df,x="FPR",y="TPR", label= 'ROC por Fluxo',color ='blue', linewidth=1.5,ax=ax).set(title="ROC - Modelo 20 Fluxos")
 sns.scatterplot(df,x="FPR",y="TPR",ax=ax,color ='blue',s=50)
-invar = sns.lineplot(x=x_bisse, y=y_bisse, sort=False, color ='red', linewidth=1.5,label="No Sense",ax=ax)
+invar = sns.lineplot(x=x_bisse, y=y_bisse, sort=False, color ='red', linewidth=1.5,label="TPR=FPR",ax=ax)
 
 line = invar.get_lines()
-plt.fill_between(line[1].get_xdata(), line[1].get_ydata(),line[0].get_ydata(), color='green', label="AUC",alpha=.12)
+plt.fill_between(line[0].get_xdata(), line[1].get_ydata(),line[0].get_ydata(), color='green', label="AUC",alpha=.12)
 
 # label points on the plot
 i=0
