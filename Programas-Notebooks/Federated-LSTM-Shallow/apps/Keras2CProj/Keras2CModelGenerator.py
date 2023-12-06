237831f2982b7ba7b08da798615c8f1c90381138
reisdout <reisdout@gmail.com>
2023-12-05 22:20:43 -0300

    evolucao do atrigo

--- Programas-Notebooks/Federated-LSTM-Shallow/apps/Keras2CProj/Keras2CModelGenerator.py
+++ Programas-Notebooks/Federated-LSTM-Shallow/apps/Keras2CProj/Keras2CModelGenerator.py
@@ -8,6 +8,8 @@ Created on Tue Nov  7 11:20:27 2023
 
 from keras2c import k2c
 
-modelPath = '/home/ns/TeseGit/Programas-Notebooks/Federated-LSTM-Shallow/apps/SigmoidProj/keras2c_model'
+modelPath = './Selected_Models/CNN_ACK_RTT/keras2c_model_CNN_ACK_ewma_RTT_ratio'
+
+
 
 k2c(modelPath, 'keras2c_model', malloc=False, num_tests=10, verbose=True)
